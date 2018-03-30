126c287b185f53765558e4c407c37a7bf02684dc
gegy1000 <gegy1000@gmail.com>
2018-03-30 19:21:18 +0200

    Event handlers

---
+++ src/main/java/net/gegy1000/psf/client/ClientEventHandler.java
@@ -0,0 +1,11 @@
+package net.gegy1000.psf.client;
+
+import net.gegy1000.psf.PracticalSpaceFireworks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod.EventBusSubscriber(modid = PracticalSpaceFireworks.MODID, value = Side.CLIENT)
+@SideOnly(Side.CLIENT)
+public class ClientEventHandler {
+}
---
+++ src/main/java/net/gegy1000/psf/server/ServerEventHandler.java
@@ -0,0 +1,8 @@
+package net.gegy1000.psf.server;
+
+import net.gegy1000.psf.PracticalSpaceFireworks;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = PracticalSpaceFireworks.MODID)
+public class ServerEventHandler {
+}
