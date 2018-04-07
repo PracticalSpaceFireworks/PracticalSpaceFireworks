cbecf10fed5597b371c1b49d46113fa75a176dfa
InsomniaKitten <admin@designdevils.co.uk>
2018-04-07 01:31:47 +0100

    Add AxisDirectionalBB and migrate BlockBattery for use with it

--- src/main/java/net/gegy1000/psf/server/block/module/BlockBattery.java
+++ src/main/java/net/gegy1000/psf/server/block/module/BlockBattery.java
@@ -1,21 +1,18 @@
 package net.gegy1000.psf.server.block.module;
 
 import net.gegy1000.psf.PracticalSpaceFireworks;
+import net.gegy1000.psf.server.util.AxisDirectionalBB;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockBattery extends BlockModule {
-    private static final AxisAlignedBB UP_BOUNDS = new AxisAlignedBB(0.25, 0.0, 0.125, 0.75, 0.09375, 0.875);
-    private static final AxisAlignedBB DOWN_BOUNDS = new AxisAlignedBB(0.25, 0.90625, 0.125, 0.75, 1.0, 0.875);
-    private static final AxisAlignedBB SOUTH_BOUNDS = new AxisAlignedBB(0.25, 0.125, 0.09375, 0.75, 0.875, 0.0);
-    private static final AxisAlignedBB NORTH_BOUNDS = new AxisAlignedBB(0.25, 0.125, 0.90625, 0.75, 0.875, 1.0);
-    private static final AxisAlignedBB EAST_BOUNDS = new AxisAlignedBB(0.09375, 0.125, 0.25, 0.0, 0.875, 0.75);
-    private static final AxisAlignedBB WEST_BOUNDS = new AxisAlignedBB(0.90625, 0.125, 0.25, 1.0, 0.875, 0.75);
+    private static final AxisDirectionalBB BOUNDING_BOX = AxisDirectionalBB.of(
+            0.25D, 0.125D, 0.90625D, 0.75D, 0.875D, 1.0D
+    );
 
     public BlockBattery(String name) {
         super(Material.CIRCUITS, name);
@@ -26,21 +23,6 @@ public class BlockBattery extends BlockModule {
 
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-        EnumFacing facing = state.getValue(DIRECTION);
-        switch (facing) {
-            case DOWN:
-                return DOWN_BOUNDS;
-            case NORTH:
-                return NORTH_BOUNDS;
-            case SOUTH:
-                return SOUTH_BOUNDS;
-            case WEST:
-                return WEST_BOUNDS;
-            case EAST:
-                return EAST_BOUNDS;
-            case UP:
-            default:
-                return UP_BOUNDS;
-        }
+        return BOUNDING_BOX.withFacing(state.getValue(DIRECTION));
     }
 }
---
+++ src/main/java/net/gegy1000/psf/server/util/AxisDirectionalBB.java
@@ -0,0 +1,56 @@
+package net.gegy1000.psf.server.util;
+
+import com.google.common.collect.Maps;
+import net.minecraft.block.Block;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
+
+import javax.annotation.Nonnull;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+
+public class AxisDirectionalBB extends AxisAlignedBB {
+    private final Map<EnumFacing, AxisAlignedBB> aabbMap = Arrays.stream(EnumFacing.VALUES)
+            .collect(Maps.toImmutableEnumMap(Function.identity(), facing -> {
+                switch (facing) {
+                    case DOWN: return new AxisAlignedBB(1 - maxX, minZ, 1 - maxY, 1 - minX, maxZ, 1 - minY);
+                    case UP: return new AxisAlignedBB(minX, 1 - maxZ, minY, maxX, 1 - minZ, maxY);
+                    case SOUTH: return new AxisAlignedBB(1 - maxX, minY, 1 - maxZ, 1 - minX, maxY, 1 - minZ);
+                    case WEST: return new AxisAlignedBB(minZ, minY, 1 - maxX, maxZ, maxY, 1 - minX);
+                    case EAST: return new AxisAlignedBB(1 - maxZ, minY, minX, 1 - minZ, maxY, maxX);
+                }
+                return this;
+            }));
+
+    private AxisDirectionalBB(double x1, double y1, double z1, double x2, double y2, double z2) {
+        super(x1, y1, z1, x2, y2, z2);
+    }
+
+    public static AxisDirectionalBB of(double x1, double y1, double z1, double x2, double y2, double z2) {
+        return new AxisDirectionalBB(x1, y1, z1, x2, y2, z2);
+    }
+
+    public static AxisDirectionalBB of(Vec3d minVec, Vec3d maxVec) {
+        return of(minVec.x, minVec.y, minVec.z, maxVec.x, maxVec.y, maxVec.z);
+    }
+
+    public static AxisDirectionalBB of(Vec3i minVec, Vec3i maxVec) {
+        return of(minVec.getX(), minVec.getY(), minVec.getZ(), maxVec.getX(), maxVec.getY(), maxVec.getZ());
+    }
+
+    public static AxisDirectionalBB of(Vec3i vec) {
+        return of(vec.getX(), vec.getY(), vec.getZ(), vec.getX() + 1, vec.getY() + 1, vec.getZ() + 1);
+    }
+
+    public static AxisDirectionalBB copyOf(AxisAlignedBB aabb) {
+        return of(aabb.minX, aabb.minY, aabb.minZ, aabb.maxX, aabb.maxY, aabb.maxZ);
+    }
+
+    public AxisAlignedBB withFacing(@Nonnull EnumFacing facing) {
+        if (facing == EnumFacing.NORTH) return this;
+        return aabbMap.getOrDefault(facing, Block.FULL_BLOCK_AABB);
+    }
+}
