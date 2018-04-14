d8c89c41c6de913cd48c379a14753f1db8f5e94d
InsomniaKitten <admin@designdevils.co.uk>
2018-04-14 01:27:27 +0100

    Improve waila integration

--- src/main/java/net/gegy1000/psf/server/compat/waila/PSFWailaPlugin.java
+++ src/main/java/net/gegy1000/psf/server/compat/waila/PSFWailaPlugin.java
@@ -6,53 +6,91 @@ import mcp.mobius.waila.api.IWailaEntityProvider;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.IWailaRegistrar;
 import mcp.mobius.waila.api.WailaPlugin;
+import mcp.mobius.waila.api.event.WailaRenderEvent;
 import mcp.mobius.waila.config.FormattingConfig;
+import net.gegy1000.psf.api.ISatellite;
 import net.gegy1000.psf.server.entity.spacecraft.EntitySpacecraft;
 import net.gegy1000.psf.server.entity.world.DelegatedWorld;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
 import java.util.List;
 
 @WailaPlugin
 public class PSFWailaPlugin implements IWailaEntityProvider, IWailaPlugin {
+    private RayTraceResult subHit;
+    private Vec3d lastHitVec;
+
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    protected void onWailaRenderPre(WailaRenderEvent.Pre event) {
+        if (event.getAccessor().getEntity() instanceof EntitySpacecraft) {
+            if (subHit == null || subHit.typeOfHit != Type.BLOCK) {
+                event.setCanceled(true);
+            }
+        }
+    }
+
     @Override
     @Nonnull
     public List<String> getWailaHead(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
-        return toSingleton(list, withFormatting(((EntitySpacecraft) entity).getSatellite().getName()));
+        if (lastHitVec != accessor.getMOP().hitVec) {
+            subHit = ((EntitySpacecraft) accessor.getEntity()).playerRayTrace(accessor.getPlayer()).orElse(null);
+        }
+        lastHitVec = accessor.getMOP().hitVec;
+        if (subHit != null && subHit.typeOfHit == Type.BLOCK) {
+            ISatellite satellite = ((EntitySpacecraft) entity).getSatellite();
+            return withValues(list, withFormatting(satellite.getName()));
+        }
+        return withValues(list);
     }
 
     @Override
     @Nonnull
     public List<String> getWailaBody(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
-        EntityPlayer player = accessor.getPlayer();
-        DelegatedWorld world = ((EntitySpacecraft) entity).getDelegatedWorld();
-        ((EntitySpacecraft) entity).playerRayTrace(player).ifPresent(result -> {
-            BlockPos pos = result.getBlockPos();
+        if (subHit != null && subHit.typeOfHit == Type.BLOCK) {
+            DelegatedWorld world = ((EntitySpacecraft) entity).getDelegatedWorld();
+            BlockPos pos = subHit.getBlockPos();
             IBlockState state = world.getBlockState(pos);
-            ItemStack stack = state.getBlock().getPickBlock(state, result, world, pos, player);
+            ItemStack stack = state.getBlock().getPickBlock(state, subHit, world, pos, accessor.getPlayer());
             if (!stack.isEmpty()) {
                 String name = withLocalization(stack.getUnlocalizedName() + ".name");
-                list.add(withLocalization("tooltip.psf.waila.looking_at" ,name));
+                return withValues(list, withLocalization("tooltip.psf.waila.looking_at", name));
             }
-        });
-        return list;
+        }
+        return withValues(list);
+    }
+
+    @Nonnull
+    @Override
+    public List<String> getWailaTail(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
+        return subHit != null && subHit.typeOfHit == Type.BLOCK ? list : withValues(list);
     }
 
     @Override
     public void register(@Nonnull IWailaRegistrar registrar) {
         registrar.registerHeadProvider(this, EntitySpacecraft.class);
         registrar.registerBodyProvider(this, EntitySpacecraft.class);
+        registrar.registerTailProvider(this, EntitySpacecraft.class);
     }
 
-    private <V> List<V> toSingleton(List<V> list, V value) {
+    @SafeVarargs
+    private final <V> List<V> withValues(List<V> list, V... values) {
         list.clear();
-        list.add(value);
+        Collections.addAll(list, values);
         return list;
     }
 
