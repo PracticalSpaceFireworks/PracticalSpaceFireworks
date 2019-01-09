6568eb2a264335cea9dec1036ae520aba4c4698e
tterrag <tterrag1098@gmail.com>
2019-01-09 02:06:35 -0500

    Fluids don't need items

--- src/main/java/net/gegy1000/psf/server/block/BlockPSFFluid.java
+++ src/main/java/net/gegy1000/psf/server/block/BlockPSFFluid.java
@@ -1,13 +1,17 @@
 package net.gegy1000.psf.server.block;
 
-import net.gegy1000.psf.server.api.RegisterItemBlock;
-import net.gegy1000.psf.server.api.RegisterItemModel;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fluids.BlockFluidFinite;
 import net.minecraftforge.fluids.Fluid;
 
-public class BlockPSFFluid extends BlockFluidFinite implements RegisterItemBlock, RegisterItemModel {
+public class BlockPSFFluid extends BlockFluidFinite {
+    
     public BlockPSFFluid(Fluid fluid, Material material) {
         super(fluid, material);
+    }
+    
+    @Override
+    public String getLocalizedName() {
+        return definedFluid.getLocalizedName(null);
     }
 }
