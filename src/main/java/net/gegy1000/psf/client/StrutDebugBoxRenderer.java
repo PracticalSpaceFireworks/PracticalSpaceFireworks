69f49fc3455aeb68f7eae05446d9fb721923c4a3
Chloe Dawn <admin@designdevils.co.uk>
2019-01-19 23:39:37 +0000

    Re-purpose debug box renderer for only struts

--- src/main/java/net/gegy1000/psf/client/StrutDebugBoxRenderer.java
+++ src/main/java/net/gegy1000/psf/client/StrutDebugBoxRenderer.java
@@ -1,6 +1,7 @@
 package net.gegy1000.psf.client;
 
 import net.gegy1000.psf.PracticalSpaceFireworks;
+import net.gegy1000.psf.server.block.module.BlockStrutAbstract;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
@@ -29,8 +30,8 @@ import java.util.List;
 
 @SideOnly(Side.CLIENT)
 @EventBusSubscriber(modid = PracticalSpaceFireworks.MODID, value = Side.CLIENT)
-public final class CollisionBoxRenderer {
-    private CollisionBoxRenderer() {}
+public final class StrutDebugBoxRenderer {
+    private StrutDebugBoxRenderer() {}
 
     @SubscribeEvent
     static void onDrawBlockHighlight(DrawBlockHighlightEvent event) {
@@ -38,17 +39,16 @@ public final class CollisionBoxRenderer {
         @Nullable RayTraceResult hit = event.getTarget();
         if (hit == null || Type.BLOCK != hit.typeOfHit) return;
         BlockPos pos = event.getTarget().getBlockPos();
-        List<AxisAlignedBB> collisionBoxes = new ArrayList<>();
         EntityPlayer player = event.getPlayer();
         float partialTicks = event.getPartialTicks();
-        AxisAlignedBB entityBox = player.getEntityBoundingBox().grow(6.0D);
         World world = player.getEntityWorld();
         IBlockState state = world.getBlockState(pos);
+        if (!(state.getBlock() instanceof BlockStrutAbstract)) return;
+        AxisAlignedBB entityBox = player.getEntityBoundingBox().grow(6.0D);
         double offsetX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
         double offsetY = player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTicks;
         double offsetZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTicks;
-        AxisAlignedBB selectedBox = state.getSelectedBoundingBox(world, pos);
-        boolean checkedSelectedBox = false;
+        List<AxisAlignedBB> collisionBoxes = new ArrayList<>();
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder builder = tessellator.getBuffer();
         GlStateManager.disableAlpha();
@@ -64,16 +64,12 @@ public final class CollisionBoxRenderer {
         builder.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION_COLOR);
         for (@Nullable AxisAlignedBB box : collisionBoxes) {
             if (box == null) continue;
-            if (!checkedSelectedBox && box.equals(selectedBox)) {
-                checkedSelectedBox = true;
-                event.setCanceled(true);
-            }
-            double minX = (box.minX - 0.002) - offsetX;
-            double minY = (box.minY - 0.002) - offsetY;
-            double minZ = (box.minZ - 0.002) - offsetZ;
-            double maxX = (box.maxX + 0.002) - offsetX;
-            double maxY = (box.maxY + 0.002) - offsetY;
-            double maxZ = (box.maxZ + 0.002) - offsetZ;
+            double minX = box.minX - offsetX;
+            double minY = box.minY - offsetY;
+            double minZ = box.minZ - offsetZ;
+            double maxX = box.maxX - offsetX;
+            double maxY = box.maxY - offsetY;
+            double maxZ = box.maxZ - offsetZ;
             RenderGlobal.drawBoundingBox(builder, minX, minY, minZ, maxX, maxY, maxZ, 0.0F, 1.0F, 0.0F, 1.0F);
         }
         tessellator.draw();
