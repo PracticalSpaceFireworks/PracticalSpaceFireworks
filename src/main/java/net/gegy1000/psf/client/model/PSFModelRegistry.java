410d3dafb4d4ca1c4299a34909a9197d5ea2abc5
Chloe Dawn <admin@designdevils.co.uk>
2019-01-19 00:52:04 +0000

    Update PSFModelRegistry

--- src/main/java/net/gegy1000/psf/client/model/PSFModelRegistry.java
+++ src/main/java/net/gegy1000/psf/client/model/PSFModelRegistry.java
@@ -7,12 +7,12 @@ import net.gegy1000.psf.server.block.controller.BlockController;
 import net.gegy1000.psf.server.block.module.BlockModule;
 import net.gegy1000.psf.server.block.module.BlockMultiblockModule;
 import net.gegy1000.psf.server.block.module.BlockPayloadSeparator;
-import net.gegy1000.psf.server.item.ItemCraftingMaterial;
-import net.gegy1000.psf.server.item.PSFItemRegistry;
 import net.gegy1000.psf.server.item.ItemCraftingMaterial.CraftingMaterial;
+import net.gegy1000.psf.server.item.PSFItemRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.client.renderer.block.statemap.IStateMapper;
 import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.item.Item;
@@ -22,66 +22,64 @@ import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.fluids.BlockFluidFinite;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nonnull;
 import java.util.Objects;
-import java.util.Set;
 
 @SideOnly(Side.CLIENT)
-@Mod.EventBusSubscriber(modid = PracticalSpaceFireworks.MODID, value = Side.CLIENT)
-public class PSFModelRegistry {
+@EventBusSubscriber(modid = PracticalSpaceFireworks.MODID, value = Side.CLIENT)
+public final class PSFModelRegistry {
     @SubscribeEvent
-    public static void onRegisterModels(ModelRegistryEvent event) {
+    static void onRegisterModels(ModelRegistryEvent event) {
         OBJLoader.INSTANCE.addDomain(PracticalSpaceFireworks.MODID);
 
-        Set<Item> registeredItems = PSFItemRegistry.getRegisteredItems();
-
-        for (Item item : registeredItems) {
-            if (item instanceof RegisterItemModel) {
-                String location = ((RegisterItemModel) item).getResource(item.getRegistryName());
-                ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(location, "inventory"));
-            }
+        for (Item item : PSFItemRegistry.getRegisteredItems()) {
+            if (!(item instanceof RegisterItemModel)) continue;
+            String path = ((RegisterItemModel) item).getResource(item.getRegistryName());
+            ModelResourceLocation model = new ModelResourceLocation(path, "inventory");
+            ModelLoader.setCustomModelResourceLocation(item, 0, model);
         }
-        
+
         for (CraftingMaterial mat : CraftingMaterial.values()) {
-            ItemCraftingMaterial craftingItem = PSFItemRegistry.craftingMaterial;
-            ModelLoader.setCustomModelResourceLocation(craftingItem, mat.ordinal(), new ModelResourceLocation(new ResourceLocation(PracticalSpaceFireworks.MODID, mat.getName()), "inventory"));
+            ResourceLocation path = new ResourceLocation(PracticalSpaceFireworks.MODID, mat.getName());
+            ModelResourceLocation model = new ModelResourceLocation(path, "inventory");
+            ModelLoader.setCustomModelResourceLocation(PSFItemRegistry.craftingMaterial, mat.ordinal(), model);
         }
 
-        Set<ItemBlock> registeredItemBlocks = PSFBlockRegistry.getRegisteredItemBlocks();
-
-        for (ItemBlock item : registeredItemBlocks) {
-            ResourceLocation name = Objects.requireNonNull(item.getRegistryName());
-            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(name, "inventory"));
+        for (ItemBlock item : PSFBlockRegistry.getRegisteredItemBlocks()) {
+            ResourceLocation path = Objects.requireNonNull(item.getRegistryName());
+            ModelResourceLocation model = new ModelResourceLocation(path, "inventory");
+            ModelLoader.setCustomModelResourceLocation(item, 0, model);
         }
 
-        IStateMapper controllerMapper = new StateMap.Builder().ignore(BlockController.TYPE).build();
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.basicController, controllerMapper);
+        ignoreProperty(PSFBlockRegistry.basicController, BlockController.TYPE);
+        ignoreProperty(PSFBlockRegistry.strut, BlockModule.DIRECTION);
+        ignoreProperty(PSFBlockRegistry.fuelTank, BlockModule.DIRECTION);
+        ignoreProperty(PSFBlockRegistry.kerosene, BlockFluidFinite.LEVEL);
+        ignoreProperty(PSFBlockRegistry.liquidOxygen, BlockFluidFinite.LEVEL);
+        ignoreProperty(PSFBlockRegistry.liquidNitrogen, BlockFluidFinite.LEVEL);
+        ignoreProperty(PSFBlockRegistry.filteredAir, BlockFluidFinite.LEVEL);
+        ignoreProperty(PSFBlockRegistry.compressedAir, BlockFluidFinite.LEVEL);
+        ignoreProperty(PSFBlockRegistry.solarPanelLarge, BlockMultiblockModule.DUMMY);
+        ignoreProperty(PSFBlockRegistry.laser, BlockMultiblockModule.DUMMY);
 
         ModelLoader.setCustomStateMapper(PSFBlockRegistry.payloadSeparator, new StateMapperBase() {
             @Override
             @Nonnull
             protected ModelResourceLocation getModelResourceLocation(@Nonnull IBlockState state) {
-                String path = "payload_separator";
-                if (state.getValue(BlockPayloadSeparator.SECURE)) path += "_secure";
-                return new ModelResourceLocation(new ResourceLocation(PracticalSpaceFireworks.MODID, path), "normal");
+                String name = "payload_separator";
+                if (state.getValue(BlockPayloadSeparator.SECURE)) name += "_secure";
+                ResourceLocation path = new ResourceLocation(PracticalSpaceFireworks.MODID, name);
+                return new ModelResourceLocation(path, "normal");
             }
         });
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.strut, new StateMap.Builder().ignore(BlockModule.DIRECTION).build());
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.fuelTank, new StateMap.Builder().ignore(BlockModule.DIRECTION).build());
+    }
 
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.kerosene, new StateMap.Builder().ignore(BlockFluidFinite.LEVEL).build());
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.liquidOxygen, new StateMap.Builder().ignore(BlockFluidFinite.LEVEL).build());
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.liquidNitrogen, new StateMap.Builder().ignore(BlockFluidFinite.LEVEL).build());
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.filteredAir, new StateMap.Builder().ignore(BlockFluidFinite.LEVEL).build());
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.compressedAir, new StateMap.Builder().ignore(BlockFluidFinite.LEVEL).build());
-        
-        IStateMapper dummyMapper = new StateMap.Builder().ignore(BlockMultiblockModule.DUMMY).build();
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.solarPanelLarge, dummyMapper);
-        ModelLoader.setCustomStateMapper(PSFBlockRegistry.laser, dummyMapper);
+    private static void ignoreProperty(Block block, IProperty<?> property) {
+        ModelLoader.setCustomStateMapper(block, new StateMap.Builder().ignore(property).build());
     }
 }
