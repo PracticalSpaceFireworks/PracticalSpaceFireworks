20dea29fdd65b2aae8cc8b1f46cf05ef8eeff8cd
Chloe Dawn <admin@designdevils.co.uk>
2019-01-20 17:45:28 +0000

    Only allow small solar panels to be placed on solid faces

--- src/main/java/net/gegy1000/psf/server/block/module/BlockSmallSolarPanel.java
+++ src/main/java/net/gegy1000/psf/server/block/module/BlockSmallSolarPanel.java
@@ -1,5 +1,6 @@
 package net.gegy1000.psf.server.block.module;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.gegy1000.psf.server.block.PSFSoundType;
 import net.gegy1000.psf.server.util.AxisDirectionalBB;
 import net.minecraft.block.material.MapColor;
@@ -10,23 +11,25 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
-import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
 
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
 public class BlockSmallSolarPanel extends BlockModule {
-    
     private static final AxisDirectionalBB BOUNDING_BOX = AxisDirectionalBB.of(0.0, 0.0, 0.75, 1.0, 1.0, 1.0);
-    
+
     public BlockSmallSolarPanel() {
         super(Material.GLASS, MapColor.LAPIS, "solar_panel_small");
         this.setSoundType(PSFSoundType.SOLAR_PANEL);
     }
-    
+
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return BOUNDING_BOX.withFacing(state.getValue(DIRECTION));
     }
-    
+
     @Override
     public MapColor getMapColor(IBlockState state, IBlockAccess world, BlockPos pos) {
         switch (state.getValue(DIRECTION)) {
@@ -38,17 +41,23 @@ public class BlockSmallSolarPanel extends BlockModule {
                 return MapColor.AIR;
         }
     }
-    
+
     @Override
-    public boolean doesSideBlockRendering(@Nonnull IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EnumFacing side) {
+    protected boolean canAttachOnSide(World world, BlockPos pos, IBlockState state, IBlockState on, EnumFacing side) {
+        BlockPos offset = pos.offset(side.getOpposite());
+        return BlockFaceShape.SOLID == world.getBlockState(offset).getBlockFaceShape(world, offset, side);
+    }
+
+    @Override
+    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing side) {
         EnumFacing facing = state.getValue(DIRECTION);
         if (side != facing.getOpposite()) {
-            final IBlockState other = world.getBlockState(pos.offset(side));
+            IBlockState other = world.getBlockState(pos.offset(side));
             return this == other.getBlock() && facing.getAxis() == other.getValue(DIRECTION).getAxis();
         }
         return true;
     }
-    
+
     @Override
     public BlockFaceShape getBlockFaceShape(IBlockAccess world, IBlockState state, BlockPos pos, EnumFacing face) {
         return face == state.getValue(DIRECTION).getOpposite() ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
