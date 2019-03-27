ca4a818f819f5c5685af2cab881ce3157958c54a
InsomniaKitten <admin@designdevils.co.uk>
2019-03-27 22:37:30 +0000

    Update PSFWailaPlugin

--- src/main/java/net/gegy1000/psf/server/compat/waila/PSFWailaPlugin.java
+++ src/main/java/net/gegy1000/psf/server/compat/waila/PSFWailaPlugin.java
@@ -1,5 +1,6 @@
 package net.gegy1000.psf.server.compat.waila;
 
+import lombok.val;
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaEntityAccessor;
 import mcp.mobius.waila.api.IWailaEntityProvider;
@@ -8,37 +9,33 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 import mcp.mobius.waila.api.WailaPlugin;
 import mcp.mobius.waila.api.event.WailaRenderEvent;
 import mcp.mobius.waila.config.FormattingConfig;
-import net.gegy1000.psf.api.ISatellite;
+import net.gegy1000.psf.PracticalSpaceFireworks;
 import net.gegy1000.psf.server.entity.spacecraft.EntitySpacecraft;
-import net.gegy1000.psf.server.entity.world.DelegatedWorld;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import javax.annotation.Nonnull;
-import java.util.Collections;
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.List;
 
 @WailaPlugin
-public class PSFWailaPlugin implements IWailaEntityProvider, IWailaPlugin {
-    private RayTraceResult subHit;
-    private Vec3d lastHitVec;
+@ParametersAreNonnullByDefault
+@EventBusSubscriber(modid = PracticalSpaceFireworks.MODID)
+public final class PSFWailaPlugin implements IWailaEntityProvider, IWailaPlugin {
+    private static final String RESET_FORMAT_CODE = "\u00a7r";
 
-    {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
+    private static RayTraceResult subHit;
+    private static Vec3d lastHitVec;
 
     @SubscribeEvent
-    protected void onWailaRenderPre(WailaRenderEvent.Pre event) {
+    static void wailaRenderPre(WailaRenderEvent.Pre event) {
         if (event.getAccessor().getEntity() instanceof EntitySpacecraft) {
-            if (subHit == null || subHit.typeOfHit != Type.BLOCK) {
+            if (subHit == null || Type.BLOCK != subHit.typeOfHit) {
                 event.setCanceled(true);
             }
         }
@@ -46,59 +43,48 @@ public class PSFWailaPlugin implements IWailaEntityProvider, IWailaPlugin {
 
     @Override
     @Nonnull
-    public List<String> getWailaHead(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
+    public List<String> getWailaHead(Entity entity, List<String> tooltip, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
         if (lastHitVec != accessor.getMOP().hitVec) {
             subHit = ((EntitySpacecraft) accessor.getEntity()).pointedBlock;
         }
         lastHitVec = accessor.getMOP().hitVec;
-        if (subHit != null && subHit.typeOfHit == Type.BLOCK) {
-            ISatellite satellite = ((EntitySpacecraft) entity).getSatellite();
-            return withValues(list, withFormatting(satellite.getName()));
+        if (subHit != null && Type.BLOCK == subHit.typeOfHit) {
+            tooltip.clear();
+            val satellite = ((EntitySpacecraft) entity).getSatellite();
+            tooltip.add(format(satellite.getName()));
         }
-        return withValues(list);
+        return tooltip;
     }
 
     @Override
     @Nonnull
-    public List<String> getWailaBody(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
-        if (subHit != null && subHit.typeOfHit == Type.BLOCK) {
-            DelegatedWorld world = ((EntitySpacecraft) entity).getBody().getWorld();
-            BlockPos pos = subHit.getBlockPos();
-            IBlockState state = world.getBlockState(pos);
-            ItemStack stack = state.getBlock().getPickBlock(state, subHit, world, pos, accessor.getPlayer());
+    public List<String> getWailaBody(Entity entity, List<String> tooltip, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
+        if (subHit != null && Type.BLOCK == subHit.typeOfHit) {
+            val world = ((EntitySpacecraft) entity).getBody().getWorld();
+            val pos = subHit.getBlockPos();
+            val state = world.getBlockState(pos);
+            val stack = state.getBlock().getPickBlock(state, subHit, world, pos, accessor.getPlayer());
             if (!stack.isEmpty()) {
-                String name = withLocalization(stack.getTranslationKey() + ".name");
-                return withValues(list, withLocalization("tooltip.psf.waila.looking_at", name));
+                tooltip.clear();
+                val name = localize(stack.getTranslationKey() + ".name");
+                tooltip.add(localize("tooltip.psf.waila.looking_at", name));
             }
         }
-        return withValues(list);
-    }
-
-    @Nonnull
-    @Override
-    public List<String> getWailaTail(Entity entity, List<String> list, IWailaEntityAccessor accessor, IWailaConfigHandler cfg) {
-        return subHit != null && subHit.typeOfHit == Type.BLOCK ? list : withValues(list);
+        return tooltip;
     }
 
     @Override
-    public void register(@Nonnull IWailaRegistrar registrar) {
+    public void register(IWailaRegistrar registrar) {
         registrar.registerHeadProvider(this, EntitySpacecraft.class);
         registrar.registerBodyProvider(this, EntitySpacecraft.class);
         registrar.registerTailProvider(this, EntitySpacecraft.class);
     }
 
-    @SafeVarargs
-    private final <V> List<V> withValues(List<V> list, V... values) {
-        list.clear();
-        Collections.addAll(list, values);
-        return list;
+    private String localize(String key, Object... args) {
+        return new TextComponentTranslation(key, args).getUnformattedComponentText();
     }
 
-    private String withLocalization(@Nonnull String key, @Nonnull Object... values) {
-        return new TextComponentTranslation(key, values).getUnformattedComponentText();
-    }
-
-    private String withFormatting(@Nonnull String string) {
-        return "\u00a7r" + String.format(FormattingConfig.entityFormat, string) + "\u00a7r";
+    private String format(String string) {
+        return RESET_FORMAT_CODE + String.format(FormattingConfig.entityFormat, string) + RESET_FORMAT_CODE;
     }
 }
