3eeaea328a389dfe9f86abfe04209906900ced8c
tterrag <tterrag1098@gmail.com>
2019-04-01 16:51:17 -0400

    Fix energy stats cap being registered to wrong class

--- src/main/java/net/gegy1000/psf/server/capability/CapabilityModuleData.java
+++ src/main/java/net/gegy1000/psf/server/capability/CapabilityModuleData.java
@@ -3,11 +3,11 @@ package net.gegy1000.psf.server.capability;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import net.gegy1000.psf.api.module.IAdditionalMass;
+import net.gegy1000.psf.api.module.IEnergyStats;
 import net.gegy1000.psf.api.module.IEntityList;
 import net.gegy1000.psf.api.module.ILaser;
 import net.gegy1000.psf.api.module.ITerrainScan;
 import net.gegy1000.psf.api.module.IWeatherData;
-import net.gegy1000.psf.server.modules.cap.EnergyStats;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -19,7 +19,7 @@ public class CapabilityModuleData {
         CapabilityManager.INSTANCE.register(IEntityList.class, new BlankStorage<>(), () -> null); // FIXME
         CapabilityManager.INSTANCE.register(ITerrainScan.class, new BlankStorage<>(), () -> null); // FIXME
         CapabilityManager.INSTANCE.register(ILaser.class, new BlankStorage<>(), () -> null); // FIXME
-        CapabilityManager.INSTANCE.register(EnergyStats.class, new BlankStorage<>(), () -> null); // FIXME
+        CapabilityManager.INSTANCE.register(IEnergyStats.class, new BlankStorage<>(), () -> null); // FIXME
         CapabilityManager.INSTANCE.register(IWeatherData.class, new BlankStorage<>(), () -> null); // FIXME
     }
 }
