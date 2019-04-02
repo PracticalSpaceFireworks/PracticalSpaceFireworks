1570e6173b889db4f8b9073d4c43a2189450fb37
InsomniaKitten <admin@designdevils.co.uk>
2019-04-02 16:03:40 +0100

    Fix GuiContainerMessage not darkening bg and not closing via keypress

--- src/main/java/net/gegy1000/psf/client/gui/GuiContainerMessage.java
+++ src/main/java/net/gegy1000/psf/client/gui/GuiContainerMessage.java
@@ -22,6 +22,9 @@ public class GuiContainerMessage extends GuiScreen {
 
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
         mc.getTextureManager().bindTexture(TEXTURE);
 
         int minX = (width - SIZE_X) / 2;
@@ -29,8 +32,6 @@ public class GuiContainerMessage extends GuiScreen {
 
         GlStateManager.color(1, 1, 1, 1);
         drawTexturedModalRect(minX, minY, 0, 0, SIZE_X, SIZE_Y);
-
-        super.drawScreen(mouseX, mouseY, partialTicks);
 
         String title = this.title.getFormattedText();
         fontRenderer.drawString(title, (width - fontRenderer.getStringWidth(title)) / 2, minY + 6, 0x404040);
@@ -45,6 +46,13 @@ public class GuiContainerMessage extends GuiScreen {
             int lineX = (width - fontRenderer.getStringWidth(line)) / 2;
             int lineY = textOriginY + textLineHeight * i;
             fontRenderer.drawString(line, lineX, lineY, 0x404040);
+        }
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) {
+        if (keyCode == 1 || mc.gameSettings.keyBindInventory.isActiveAndMatches(keyCode)) { 
+            mc.player.closeScreen(); 
         }
     }
 
